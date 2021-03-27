a3f3717ab3c0827fff8c4d18a89d2539e86bbd16
Chae, Junghoon <chaej@ornl.gov>
2021-03-27 00:19:14 -0400

    update

--- assets/js/publication_k.js
+++ assets/js/publication_k.js
@@ -1,5 +1,8 @@
+/**
+ * Created by karthik on 10/29/16.
+ */
 
-Array.prototype.unique = function () {
+ Array.prototype.unique = function () {
     var a = this.concat();
     for (var i = 0; i < a.length; ++i) {
         for (var j = i + 1; j < a.length; ++j) {
@@ -27,18 +30,18 @@ BarChart.prototype.drawHorizontalBar = function (key, top) {
 
 $(document).ready(function () {
 
-    d3.json("/assets/data/publications.json").then(data=> {
-        
+    d3.json("/assets/data/publications.json").then(data=>{
+
         // Empty the div
         d3.select("#publicationsViz").empty();
         d3.select("#publicationsList").empty();
-
+        
         //create publications viz
         publicationsViz = new BarChart ({
             id: "#publicationsViz",
             data: data
         });
-
+        
         var publicationsList = d3.select("#publicationsList").style("float", "left");
 
         // meta data
@@ -68,24 +71,25 @@ $(document).ready(function () {
 
         // publicationsList.append("div").html("Karthik has <b>" + data.length + " peer-reviewed publications</b> with <b>" + (coauthors.length - 1) + " collaborators</b> since " + data[data.length-1].year + ".")
 
-        publicationsList.append("div").html("<br/>");
+        // publicationsList.append("div").html("<br/>");
+        // publicationsList.append("div").html('<p>First author contributions are highlighted with a <span style="background-color:#f0f0f0; border:1px solid white;">gray background.</span></p>');
 
         // publications
         publicationsContent = publicationsList.append("div").attr("id", "publicationsContent");
 
-        // publicationsContent.append("h3").text("Peer-Reviewed Journal Papers (" + journals.length + ")");
+        publicationsContent.append("h3").text("Peer-Reviewed Journal Papers (" + journals.length + ")");
         journals.forEach(function (paper, i) {
             showPublication(publicationsContent, paper, i);
 
         });
 
-        // publicationsContent.append("h3").html("Peer-Reviewed Conference Papers (" + conferences.length + ")");
+        publicationsContent.append("h3").html("Peer-Reviewed Conference Papers (" + conferences.length + ")");
         conferences.forEach(function (paper, i) {
             showPublication(publicationsContent, paper, i);
         });
 
 
-        // publicationsContent.append("h3").html("Featured Workshop Papers");
+        publicationsContent.append("h3").html("Featured Workshop Papers");
         workshops.forEach(function (paper, i) {
             showPublication(publicationsContent, paper, i);
         });
@@ -99,7 +103,9 @@ function showPublication (publicationsContent, paper, i) {
         .style("width", "100%")
         .style("class", "archive");
 
-    var pub = publicationsContent.append("div").style("display", "block")
+    var pub = publicationsContent.append("div").style("display", "flex")
+        .style("justify-content", "center")
+        .style("align-items", "center")
         .style("padding-left", "10px")
         .style("line-height", "1")
         .style("padding-bottom", "5px")
@@ -107,23 +113,24 @@ function showPublication (publicationsContent, paper, i) {
         .style("padding-top", "5px")
         .style("background", "white");
 
-    pub.append("div").style("width", "120px")
+    pub.append("a").attr("href", paper.pdf).attr("target", "_blank").append("div").style("width", "120px")
         .style("height", "80px")
         .style("display", "inline-block")
         .style("border", "1.5px solid #222")
         .style("background-size", "cover")
         .style("background-repeat", "no-repeat")
         .style("vertical-align", "top")
-        .style("background-image", "url(/assets/data/images/" + paper.id + ".png)");
+        .style("background-image", "url(/assets/data/images/" + paper.id + ".png)")
+        
 
-    var pubInfo = pub.append("div").style("width", "calc(100% - 110px)")
+    var pubInfo = pub.append("div").style("width", "calc(100% - 120px)")
         .style("height", "100%")
         .style("background", "transparent")
         .style("padding-left", "10px")
         // .style("height", "72px")
         .style("display", "inline-block");
 
-    if ("award" in paper) {
+     if ("award" in paper) {
         var award = pubInfo.append("div").style("float", "right").style("height", "auto");
         // award.append("span").html("<b>" + paper["award"].toUpperCase() + "</b>").style("font-size", "12px");
         award.append("img").attr("src", "/assets/images/badge.png").style("height", "25px").style("margin-right", "3px");
@@ -136,7 +143,7 @@ function showPublication (publicationsContent, paper, i) {
 
     paper.authors.forEach(function (author, j) {
         var mainAuthor = false;
-        if (author == "Sriram Karthik Badam") {
+        if (author == "Junghoon Chae") {
             author = "" + author + "";
             mainAuthor = true;
             if (j == 0)
@@ -150,11 +157,11 @@ function showPublication (publicationsContent, paper, i) {
         // }
 
         pubInfo.append("span").html(function ()
-            {
-                return mainAuthor? "<b>" + author + "</b>" : author;
-            }
+        {
+            return mainAuthor? "<b>" + author + "</b>" : author;
+        }
         ).style("font-size", "12px")
-        // .style("background-color", mainAuthor?"#f0e8ff":"transparent");
+            // .style("background-color", mainAuthor?"#f0e8ff":"transparent");
 
         if (j != paper.authors.length - 1) {
 
@@ -180,16 +187,22 @@ function showPublication (publicationsContent, paper, i) {
     pubInfo.append("span").html(paper.venue + ", " + paper.year +"<br/>").style("font-size", "12px");
 
     if (paper.pdf != "") {
-        pubInfo.append("span").attr("class", "textlink").html('<a target="_blank" href="' + paper.pdf + '">[pdf]</a>  ').style("font-size", "12px");
+        pubInfo.append("span").attr("class", "textlink").html('<a target="_blank" href="' + paper.pdf + '">[pdf]</a>').style("font-size", "12px");
     }
 
-    if (typeof paper.video != 'undefined' && paper.video != "") {
+    if (paper.video != "" && typeof paper.video != 'undefined') {
         pubInfo.append("span").attr("class", "textlink").html('<a target="_blank" href="' + paper.video + '">[video]</a>  ').style("font-size", "12px");
     }
 
-    if ("award" in paper) {
+     if ("award" in paper) {
         var award = pubInfo.append("div").style("float", "right").style("height", "auto");
         award.append("span").html("<b>" + paper["award"].toUpperCase() + "</b>").style("font-size", "12px").style("margin-right", "3px");
         // award.append("img").attr("src", "/assets/images/badge.png").style("height", "25px").style("margin-right", "3px");
     }
+
+    //
+    // if (paper.bibtex != "") {
+    //     pubInfo.append("span").attr("class", "textlink").html('<a target="_blank" href="' + paper.bibtex + '">(bibtex)</a> ').style("font-size", "12px");
+    // }
+
 }
